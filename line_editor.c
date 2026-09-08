0400a11552f583618a424e537beb4f2ff8357180
Akshitha N <akshgowdru@gmal.com>
2026-09-08 12:15:36 +0530

    Initial line editor project

---
+++ line_editor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINES 100
+#define MAX_LENGTH 200
+
+char lines[MAX_LINES][MAX_LENGTH];
+int count = 0;
+
+void insertLine();
+void deleteLine();
+void displayDocument();
+
+int main()
+{
+    printf("Simple Line Editor\n");
+
+    return 0;
+}
+
+void insertLine()
+{
+    // Insert line code will come here
+}
+
+void deleteLine()
+{
+    // Delete line code will come here
+}
+
+void displayDocument()
+{
+    // Display code will come here
+}
